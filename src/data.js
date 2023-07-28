bf38ffe44c2aeaf285891349b7148ac88981def8
Larissa S <vtcontato@gmail.com>
2023-07-27 23:05:12 -0300

    teste commit

---
+++ src/data.js
@@ -0,0 +1,46 @@
+const list = [
+  {
+    id: 1,
+    title: "Conjunto de Tintas Aquarelas",
+    price: 93,
+    img: "https://m.media-amazon.com/images/I/716sZvbRHaL._AC_SL1024_.jpg",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "Conjunto De Pincéis De Pintura Plana",
+    price: 57,
+    img: "https://m.media-amazon.com/images/I/71xPh6LGMYL._AC_SL1500_.jpg",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Conjunto de ferramentas de pontilhar",
+    price: 78,
+    img: "https://m.media-amazon.com/images/I/61ONZ9eSp6L._AC_SL1024_.jpg",
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: "5 peças de espátulas de pintura",
+    price: 69,
+    img: "https://m.media-amazon.com/images/I/61tEYhbNEKL._AC_SL1500_.jpg",
+    amount: 1,
+  },
+  {
+    id: 5,
+    title: "Pincel de pintura a óleo",
+    price: 54,
+    img: "https://m.media-amazon.com/images/I/613mjGZJmXS._AC_SL1001_.jpg",
+    amount: 1,
+  },
+  {
+    id: 6,
+    title: "Conjunto 12 Pincéis De Pintura",
+    price: 40,
+    img: "https://m.media-amazon.com/images/I/61auCwi2yEL._AC_SL1024_.jpg",
+    amount: 1,
+  },
+];
+
+export default list;
